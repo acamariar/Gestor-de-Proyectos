adea0e7705a46202e0539c7dc2a9933145b12bae
maria romero <mariaenriquetaromero1997@gmail.com>
2025-08-13 21:13:12 -0300

    agregando diferentes estados a las tareas

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,15 +1,28 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
-
+const taskStatus = {
+  PENDING: "pending",
+  ON_HOLD: "onhold",
+  IN_PROGRESS: "inprogress",
+  UNDER_REVIEW: "underreview",
+  COMPLETED: "completed",
+} as const;
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus];
 export interface ITask extends Document {
   name: string;
   description: string;
   project: Types.ObjectId;
+  status: TaskStatus;
 }
 export const TaskSchema: Schema = new Schema(
   {
     name: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
     project: { type: Types.ObjectId, ref: "project" },
+    status: {
+      type: String,
+      enum: Object.values(taskStatus),
+      default: taskStatus.PENDING,
+    },
   },
   { timestamps: true }
 );
